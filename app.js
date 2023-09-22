369c3986f7bdc2d9a42e342ce3059b0def692d35
EddoDeuz <edgardojnx@outlook.com>
2023-09-21 22:22:22 -0300

    agregue link directo

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
     router(req.url, res)
   
-}).listen(3030, 'localhost', () => console.log('Server running in 3030 port'));
+}).listen(3030, 'localhost', () => console.log('Server running in 3030 port:http://localhost:3030'));
